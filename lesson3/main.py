4bc81f9cb72752579e64837bc17c4f1b97d15f71
cartooonycatboys <cimsitruzgar@gmail.com>
2026-01-16 21:02:35 +0300

    Add files via upload

---
+++ lesson3/main.py
@@ -0,0 +1,34 @@
+import pygame
+import time
+pygame.init()
+screen=pygame.display.set_mode((600,600))
+pygame.display.set_caption(("birthday greeting card"))
+
+img=pygame.image.load("images/image1.jpg")
+image=pygame.transform.scale(img,(600,600))
+
+img2=pygame.image.load("images/image2.jpg")
+image2=pygame.transform.scale(img2,(600,600))
+
+
+
+font=pygame.font.SysFont("times new roman",72)
+text=font.render("Happy",True,("black"))
+text2=font.render("Birthday",True,("black"))
+text3=font.render("many happy returns of the day",True,("black"))
+
+while True:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            pygame.quit()
+    screen.blit(image,(0,0))
+    screen.blit(text,(200,200))
+    screen.blit(text2,(200,290))
+    pygame.display.update()
+    time.sleep(2)
+    print("working")
+    screen.fill("white")
+    screen.blit(image2,(0,0))
+    screen.blit(text3,(200,200))
+    pygame.display.update()
+    time.sleep(2)
