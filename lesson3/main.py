e6d7d0cc3fc0b4d72ec8eebf7b781270294ab9e0
cartooonycatboys <cimsitruzgar@gmail.com>
2026-01-23 20:21:52 +0300

    Add files via upload

--- lesson3/main.py
+++ lesson3/main.py
@@ -10,12 +10,16 @@ image=pygame.transform.scale(img,(600,600))
 img2=pygame.image.load("images/image2.jpg")
 image2=pygame.transform.scale(img2,(600,600))
 
-
+img3=pygame.image.load("images/image3.jpg")
+image3=pygame.transform.scale(img3,(600,600))
 
 font=pygame.font.SysFont("times new roman",72)
 text=font.render("Happy",True,("black"))
 text2=font.render("Birthday",True,("black"))
+font=pygame.font.SysFont("times new roman",30)
 text3=font.render("many happy returns of the day",True,("black"))
+font=pygame.font.SysFont("times new roman",72)
+text4=font.render("happy suprises",True,("black"))
 
 while True:
     for event in pygame.event.get():
@@ -29,6 +33,10 @@ while True:
     print("working")
     screen.fill("white")
     screen.blit(image2,(0,0))
-    screen.blit(text3,(200,200))
+    screen.blit(text3,(150,235))
+    pygame.display.update()
+    time.sleep(2)
+    screen.blit(image3,(0,0))
+    screen.blit(text4,(100,300))
     pygame.display.update()
     time.sleep(2)
