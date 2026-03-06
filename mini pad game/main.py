0c80960c760c73c85e0519861c383efa4cd5b013
cartooonycatboys <cimsitruzgar@gmail.com>
2026-03-06 20:54:00 +0300

    Add files via upload

--- mini pad game/main.py
+++ mini pad game/main.py
@@ -1,17 +1,48 @@
 import pygame
+import random
+import time
 pygame.init()
 
 screen=pygame.display.set_mode((900,1000))
 squares=[
-    pygame.Rect(100, 100, 50, 50),pygame.Rect(300, 100, 50, 50),
-    pygame.Rect(100, 300, 50, 50),pygame.Rect(300, 300, 50, 50) 
+    pygame.Rect(100, 100, 300, 300),pygame.Rect(500, 100, 300, 300),
+    pygame.Rect(100, 500, 300, 300),pygame.Rect(500, 500, 300, 300) 
     ]
+sequence=[]
+playerinput=[]
 
+showing_sequence=True
+
+def drawsquares(highliteindex=None):
+    screen.fill("blue")
+    for i in range(4):
+        if i==highliteindex:
+            pygame.draw.rect(screen,'#3d3c3c',squares[i])
+        else:
+            pygame.draw.rect(screen,"#ffffff",squares[i])
+    pygame.display.update()
+def showsequence():
+    global showing_sequence
+    for i in sequence:
+        drawsquares(i)
+        pygame.time.delay(600)
+        drawsquares()
+        pygame.time.delay(300)
+    showing_sequence=False
+sequence.append(random.randint(0,3))
+clock = pygame.time.Clock()
 while True:
+    clock.tick(60)
+    if showing_sequence:
+        showsequence()
+        playerinput=[]
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
-    screen.fill((118,118,118))
-    for i in range(4):
-        pygame.draw.rect(screen,'#3d3c3c',squares[i])
-    pygame.display.update()
+        if event.type==pygame.MOUSEBUTTONDOWN and not showing_sequence:
+            for i in range (4):
+                if squares[i].collidepoint(event.pos):
+                    playerinput.append(i)
+    drawsquares()
+
+    
